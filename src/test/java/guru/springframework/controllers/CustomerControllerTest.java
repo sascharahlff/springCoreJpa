dc820cc1c77449fb39b5e0a73e3a1ae8b047609b
Sascha Rahlff <srRahlff@itemis.com>
2018-05-24 09:46:52 +0200

    Add JPA embedded entities.

--- src/test/java/guru/springframework/controllers/CustomerControllerTest.java
+++ src/test/java/guru/springframework/controllers/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Address;
 import guru.springframework.domain.Customer;
 import guru.springframework.services.CustomerService;
 import org.junit.Before;
@@ -104,11 +105,12 @@ public class CustomerControllerTest {
         returnCustomer.setId(id);
         returnCustomer.setFirstName(firstName);
         returnCustomer.setLastName(lastName);
-        returnCustomer.setAddressLine1(addressLine1);
-        returnCustomer.setAddressLine2(addressLine2);
-        returnCustomer.setCity(city);
-        returnCustomer.setState(state);
-        returnCustomer.setZipCode(zipCode);
+        returnCustomer.setBillingAddress(new Address());
+        returnCustomer.getBillingAddress().setAddressLine1(addressLine1);
+        returnCustomer.getBillingAddress().setAddressLine2(addressLine2);
+        returnCustomer.getBillingAddress().setCity(city);
+        returnCustomer.getBillingAddress().setState(state);
+        returnCustomer.getBillingAddress().setZipCode(zipCode);
         returnCustomer.setEmail(email);
         returnCustomer.setPhoneNumber(phoneNumber);
 
@@ -118,11 +120,11 @@ public class CustomerControllerTest {
         .param("id", "1")
         .param("firstName", firstName)
         .param("lastName", lastName)
-        .param("addressLine1", addressLine1)
-        .param("addressLine2", addressLine2)
-        .param("city", city)
-        .param("state", state)
-        .param("zipCode", zipCode)
+        .param("shippingAddress.addressLine1", addressLine1)
+        .param("shippingAddress.addressLine2", addressLine2)
+        .param("shippingAddress.city", city)
+        .param("shippingAddress.state", state)
+        .param("shippingAddress.zipCode", zipCode)
         .param("email", email)
         .param("phoneNumber", phoneNumber))
                 .andExpect(status().is3xxRedirection())
@@ -130,11 +132,11 @@ public class CustomerControllerTest {
                 .andExpect(model().attribute("customer", instanceOf(Customer.class)))
                 .andExpect(model().attribute("customer", hasProperty("firstName", is(firstName))))
                 .andExpect(model().attribute("customer", hasProperty("lastName", is(lastName))))
-                .andExpect(model().attribute("customer", hasProperty("addressLine1", is(addressLine1))))
-                .andExpect(model().attribute("customer", hasProperty("addressLine2", is(addressLine2))))
-                .andExpect(model().attribute("customer", hasProperty("city", is(city))))
-                .andExpect(model().attribute("customer", hasProperty("state", is(state))))
-                .andExpect(model().attribute("customer", hasProperty("zipCode", is(zipCode))))
+                .andExpect(model().attribute("customer", hasProperty("shippingAddress", hasProperty("addressLine1", is(addressLine1)))))
+                .andExpect(model().attribute("customer", hasProperty("shippingAddress", hasProperty("addressLine2", is(addressLine2)))))
+                .andExpect(model().attribute("customer", hasProperty("shippingAddress", hasProperty("city", is(city)))))
+                .andExpect(model().attribute("customer", hasProperty("shippingAddress", hasProperty("state", is(state)))))
+                .andExpect(model().attribute("customer", hasProperty("shippingAddress", hasProperty("zipCode", is(zipCode)))))
                 .andExpect(model().attribute("customer", hasProperty("email", is(email))))
                 .andExpect(model().attribute("customer", hasProperty("phoneNumber", is(phoneNumber))));
 
@@ -146,11 +148,11 @@ public class CustomerControllerTest {
         assertEquals(id, boundCustomer.getId());
         assertEquals(firstName, boundCustomer.getFirstName());
         assertEquals(lastName, boundCustomer.getLastName());
-        assertEquals(addressLine1, boundCustomer.getAddressLine1());
-        assertEquals(addressLine2, boundCustomer.getAddressLine2());
-        assertEquals(city, boundCustomer.getCity());
-        assertEquals(state, boundCustomer.getState());
-        assertEquals(zipCode, boundCustomer.getZipCode());
+        assertEquals(addressLine1, boundCustomer.getShippingAddress().getAddressLine1());
+        assertEquals(addressLine2, boundCustomer.getShippingAddress().getAddressLine2());
+        assertEquals(city, boundCustomer.getShippingAddress().getCity());
+        assertEquals(state, boundCustomer.getShippingAddress().getState());
+        assertEquals(zipCode, boundCustomer.getShippingAddress().getZipCode());
         assertEquals(email, boundCustomer.getEmail());
         assertEquals(phoneNumber, boundCustomer.getPhoneNumber());
 
